752a4b834a2b9fffc7cf0f50a3d8780ad5df0761
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-02-23 13:43:35 +0300

    Numbers: create PrimeNumbers

---
+++ Numbers/src/PrimeNumbers.java
@@ -0,0 +1,20 @@
+// Finds prime numbers between 1 and 100
+
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        boolean isDividable = false;
+
+
+    NEXT_NUMBER: for (int number = 2; number < 100; number++ ) {
+                    for (int divisor = 2; divisor < number; divisor++) {
+                        if (number % divisor == 0) {
+                            isDividable = true;
+                            continue NEXT_NUMBER;
+                        }
+                    }
+                    System.out.print(number + " ");
+                    }
+    }
+    }
