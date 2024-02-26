d0f68195e6a8ab360672b60091ae4b670f8a16ff
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-02-26 20:08:28 +0300

    refactor Numbers: Power using recursive method

--- Numbers/src/Power.java
+++ Numbers/src/Power.java
@@ -1,24 +1,38 @@
-// Finds exponents of given positive number
+// Finds power of given number using a static method recursively
 
 import java.util.Scanner;
 
 public class Power {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
         int base, exponent;
-        int result = 1;
+
+        base = askBaseNumber();
+        exponent = askExponent();
+
+        System.out.print("Result: " + power(base, exponent));
+    }
+
+    // step down to function's 1st element: f(0) => 1
+    // step up to function's last element: f(n) => n * f(n-1)
+    static int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
+
+    static int askBaseNumber() {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Provide an integer for the base: ");
-        base = input.nextInt();
+        return scanner.nextInt();
+    }
 
-        System.out.print("Provide a positive integer for the exponent: ");
-        exponent = input.nextInt();
+    static int askExponent() {
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i=1; i <= exponent; i++) {
-            result *= base;
-        }
-
-        System.out.print("Result: " + result);
+        System.out.print("Provide an integer for the exponent: ");
+        return scanner.nextInt();
     }
 }
