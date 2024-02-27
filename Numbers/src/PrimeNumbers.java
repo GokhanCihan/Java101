b8cc2401085a844febbd23b63a3f46b55fa8c5f6
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-02-27 16:19:32 +0300

    refactor Numbers: PrimeNumbers using recursive method

--- Numbers/src/PrimeNumbers.java
+++ Numbers/src/PrimeNumbers.java
@@ -1,20 +1,37 @@
 // Finds prime numbers between 1 and 100
 
+import java.util.Scanner;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
+        int number;
 
-        boolean isDividable = false;
+        number = askNumber();
+        System.out.println(isPrime(number));
+    }
 
+    static int askNumber() {
+        Scanner scanner = new Scanner(System.in);
 
-        NEXT_NUMBER: for (int number = 2; number < 100; number++ ) {
-            for (int divisor = 2; divisor < number; divisor++) {
-                if (number % divisor == 0) {
-                    isDividable = true;
-                    continue NEXT_NUMBER;
-                }
+        System.out.print("Provide an integer: ");
+        return scanner.nextInt();
+    }
+    
+    static boolean isPrime(int number) {
+        return checkNotDividable(number, 2);
+    }
+
+    // increment divisor
+    // if divisor reaches to the number it means that
+    // the number is not dividable other than itself
+    static boolean checkNotDividable(int number, int divisor) {
+        if (divisor == number) {
+            return true;
+        } else {
+            if (number % divisor == 0) {
+                return false;
             }
-            System.out.print(number + " ");
+            return checkNotDividable(number, divisor + 1);
         }
     }
-    }
+}
